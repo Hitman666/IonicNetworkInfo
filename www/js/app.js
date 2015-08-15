6f4c41dd51187bb04742c19e952e69a503fb39e5
Nikola Breznjak <nikola.breznjak@gmail.com>
2015-08-15 10:24:47 +0200

    finished test project

---
+++ www/js/app.js
@@ -0,0 +1,47 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic', 'ngCordova'])
+
+.run(function($ionicPlatform, $cordovaNetwork, $rootScope) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+
+  });
+})
+
+.controller('MyCtrl', function($scope, $cordovaNetwork, $rootScope) {
+    document.addEventListener("deviceready", function () {
+
+        $scope.network = $cordovaNetwork.getNetwork();
+        $scope.isOnline = $cordovaNetwork.isOnline();
+        $scope.$apply();
+        
+        // listen for Online event
+        $rootScope.$on('$cordovaNetwork:online', function(event, networkState){
+            $scope.isOnline = true;
+            $scope.network = $cordovaNetwork.getNetwork();
+            
+            $scope.$apply();
+        })
+
+        // listen for Offline event
+        $rootScope.$on('$cordovaNetwork:offline', function(event, networkState){
+            console.log("got offline");
+            $scope.isOnline = false;
+            $scope.network = $cordovaNetwork.getNetwork();
+            
+            $scope.$apply();
+        })
+
+  }, false);
+});
